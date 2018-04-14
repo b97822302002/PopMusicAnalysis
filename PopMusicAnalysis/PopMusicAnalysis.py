830c68593b115f2d349c7c799840cb498c55e875
b97822302002 <b97822302002@users.noreply.github.com>
2018-04-15 01:54:37 +0800

    使用SubProcess呼叫MuseScore進行Midi轉MusicXML

--- PopMusicAnalysis/PopMusicAnalysis.py
+++ PopMusicAnalysis/PopMusicAnalysis.py
@@ -1,1 +1,13 @@
+from os.path import join, basename
+import subprocess, glob
 
+musescoreExePath = r"C:\Program Files (x86)\MuseScore 2\bin\MuseScore.exe"
+midiFileDirectoryPath = r".\data_midi"
+targetMusicXmlDirectoryPath = r".\data_musicxml"
+
+for midiFile in glob.glob(join(midiFileDirectoryPath, "*.mid")):
+    subprocess.run( \
+        [musescoreExePath, \
+         join(midiFileDirectoryPath, basename(midiFile)), \
+         "--export-to", \
+         join(targetMusicXmlDirectoryPath, basename(midiFile)[:-4]+".musicxml")])
